f8bb9b433d3604842325fe5f00d41dc95a6e64cd
Johan Vasquez <johanjvc7@gmail.com>
2022-11-24 08:38:07 -0500

    Backend inicializado

---
+++ Opticol/src/main/java/com/MinTic/Opticol/OpticolApplication.java
@@ -0,0 +1,13 @@
+package com.MinTic.Opticol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticolApplication.class, args);
+	}
+
+}
---
+++ Opticol/src/test/java/com/MinTic/Opticol/OpticolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MinTic.Opticol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
