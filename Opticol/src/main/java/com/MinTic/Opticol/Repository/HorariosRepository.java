eb1df5ff6cb6d26ae3ff2d0fd5f51763e56c3ea4
Johan Vasquez <johanjvc7@gmail.com>
2022-11-27 09:52:12 -0500

    Actualización Pom - Error con Tomcat

--- Opticol/src/main/java/com/MinTic/Opticol/Repository/HorariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MinTic.Opticol.Repository;
-
-import com.MinTic.Opticol.Entities.Horario;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HorariosRepository extends MongoRepository<Horario, String> {
-}
