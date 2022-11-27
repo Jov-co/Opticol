dd3cf6aef625474fa4af9f7a3bf65f64cf63251c
Rafael Solano <rafaelsolanol007@gmail.com>
2022-11-27 10:37:27 -0500

    Modelo  Cita
    Se agrega Cita  service, impl, cotroler

--- Opticol/src/main/java/com/MinTic/Opticol/Services/EmpleadosService.java
+++ Opticol/src/main/java/com/MinTic/Opticol/Services/EmpleadosService.java
@@ -2,7 +2,9 @@ package com.MinTic.Opticol.Services;
 
 
 import com.MinTic.Opticol.Entities.Empleados;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -18,4 +20,17 @@ public interface EmpleadosService {
 
     public ResponseEntity<Empleados> actualizarEmpleado(Empleados empleado);
 
+    @Service
+    interface PacienteService  {
+
+        public List<Pacientes> listar();
+        public ResponseEntity<Object> guardar (Pacientes paciente, HttpHeaders headers);
+
+        public ResponseEntity<Pacientes> buscarPorId (String id);
+
+        public ResponseEntity<Pacientes> modificarPaciente(Pacientes paciente, HttpHeaders headers);
+        public boolean existsById (String id);
+
+        public ResponseEntity<Pacientes>eliminarPaciente(String id);
+    }
 }
